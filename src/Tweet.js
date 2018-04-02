397fb8e854d7af88a1a830270be81dbd517d1352
harenarium <22226614+harenarium@users.noreply.github.com>
2018-04-02 10:38:17 -0400

    Done.

---
+++ src/Tweet.js
@@ -0,0 +1,17 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+class Tweet extends React.Component {
+  render() {
+    return (
+      <div className="tweet">
+        <img src="http://twitter.com/some-avatar.png" className="tweet__avatar" />
+        <div className="tweet__body">
+          <p>We are writing this tweet in JSX. Holy moly!</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Tweet;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 // Code goes here
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { message } from './components/foo';
+
+import fruit from './fruits';
+
+import Tweet from './Tweet';
+
+// ReactDOM.render(
+//   <Tweet />,
+//   document.getElementById('root')
+// );
+
+console.log(fruit.apple); // prints 'red'
+
+// // In another file, also in the same directory
+// import { apple } from './fruits';
+// console.log(apple); // prints 'red'
